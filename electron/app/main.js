44a5b45bf12524ab6a0486161d69a5eb71b05a7b
707 <707472783@qq.com>
2016-06-23 17:28:07 +0800

    use photon

--- electron/app/main.js
+++ electron/app/main.js
@@ -1,11 +1,12 @@
 const electron = require('electron');
 const {app} = electron;
 const {BrowserWindow} = electron;
+const {ipcMain} = electron;
 
 let mainWindow;
 
 function createWindow() {
-    mainWindow = new BrowserWindow({width:400, height:600});
+    mainWindow = new BrowserWindow({width:400, height:200, autoHideMenuBar:true});
     mainWindow.loadURL(`file://${__dirname}/index.html`);
     mainWindow.on('closed', () => {
         mainWindow = null;
@@ -24,4 +25,8 @@ app.on('activate', () => {
     if (mainWindow === null) {
         createWindow();
     }
+});
+
+ipcMain.on('asynchronous-message', function (event, arg) {
+    mainWindow.setContentSize(400, 600);
 });
